Fusion3Design/F3slic3r

commit fa86a3d0470c736a5792df2c8296e0945a169a21

Searcher related refactoring:
* Searcher moved form Plater to Gui_App

src/slic3r/GUI/GUI_App.cpp
@@ -1094,6 +1094,61 @@ bool GUI_App::OnInit()
     }
 }
 
+void GUI_App::check_and_update_searcher(ConfigOptionMode mode /*= comExpert*/)
+{
+    std::vector<Search::InputInfo> search_inputs{};
+
+    auto print_tech = preset_bundle->printers.get_selected_preset().printer_technology();
+    for (auto tab : tabs_list)
+        if (tab->supports_printer_technology(print_tech))
+            search_inputs.emplace_back(Search::InputInfo{ tab->get_config(), tab->type() });
+
+    m_searcher.check_and_update(print_tech, mode, search_inputs);
+}
+
+void GUI_App::jump_to_option(const std::string& opt_key, Preset::Type type, const std::wstring& category)
+{
+    get_tab(type)->activate_option(opt_key, category);
+}
+
+void GUI_App::jump_to_option(size_t selected)
+{
+    const Search::Option& opt = m_searcher.get_option(selected);
+    if (opt.type == Preset::TYPE_PREFERENCES)
+        open_preferences(opt.opt_key(), boost::nowide::narrow(opt.group));
+    else
+        get_tab(opt.type)->activate_option(opt.opt_key(), boost::nowide::narrow(opt.category));
+}
+
+void GUI_App::jump_to_option(const std::string& composite_key)
+{
+    const auto        separator_pos = composite_key.find(";");
+    const std::string opt_key       = composite_key.substr(0, separator_pos);
+    const std::string tab_name      = composite_key.substr(separator_pos + 1, composite_key.length());
+
+    for (Tab* tab : tabs_list) {
+        if (tab->name() == tab_name) {
+            check_and_update_searcher();
+
+            // Regularly searcher is sorted in respect to the options labels,
+            // so resort searcher before get an option
+            m_searcher.sort_options_by_key();
+            const Search::Option& opt = m_searcher.get_option(opt_key, tab->type());
+            tab->activate_option(opt_key, boost::nowide::narrow(opt.category));
+
+            // Revert sort of searcher back
+            m_searcher.sort_options_by_label();
+            break;
+        }
+    }
+}
+
+void GUI_App::show_search_dialog()
+{
+    check_and_update_searcher(get_mode());
+    m_searcher.show_dialog();
+}
+
 static int get_app_font_pt_size(const AppConfig* app_config)
 {
     if (!app_config->has("font_pt_size"))

src/slic3r/GUI/GUI_App.hpp
@@ -12,6 +12,7 @@
 #include "ConfigWizard.hpp"
 #include "OpenGLManager.hpp"
 #include "libslic3r/Preset.hpp"
+#include "Search.hpp"
 
 #include <wx/app.h>
 #include <wx/colour.h>
@@ -175,6 +176,8 @@ private:
     std::string m_instance_hash_string;
 	size_t m_instance_hash_int;
 
+    Search::OptionsSearcher m_searcher;
+
 public:
     bool            OnInit() override;
     bool            initialized() const { return m_initialized; }
@@ -187,6 +190,14 @@ public:
     bool is_gcode_viewer() const { return m_app_mode == EAppMode::GCodeViewer; }
     bool is_recreating_gui() const { return m_is_recreating_gui; }
     std::string logo_name() const { return is_editor() ? "PrusaSlicer" : "PrusaSlicer-gcodeviewer"; }
+
+    Search::OptionsSearcher& searcher() noexcept { return m_searcher; }
+    void                     check_and_update_searcher(ConfigOptionMode mode = comExpert);
+    void                     jump_to_option(size_t selected);
+    void                     jump_to_option(const std::string& opt_key, Preset::Type type, const std::wstring& category);
+    // jump to option which is represented by composite key : "opt_key;tab_name"
+    void                     jump_to_option(const std::string& composite_key);
+    void                     show_search_dialog();
 
     // To be called after the GUI is fully built up.
     // Process command line parameters cached in this->init_params,

src/slic3r/GUI/MainFrame.cpp
@@ -288,6 +288,11 @@ DPIFrame(NULL, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_S
 
         preferences_dialog = new PreferencesDialog(this);
     }
+
+    if (wxGetApp().is_editor()) {
+        // jump to found option from SearchDialog
+        Bind(wxCUSTOMEVT_JUMP_TO_OPTION, [this](wxCommandEvent& evt) { wxGetApp().jump_to_option(evt.GetInt()); });
+    }
 }
 
 #ifdef _MSW_DARK_MODE
@@ -1054,6 +1059,8 @@ void MainFrame::on_dpi_changed(const wxRect& suggested_rect)
     this->SetSize(sz);
 
     this->Maximize(is_maximized);
+
+    wxGetApp().searcher().dlg_msw_rescale();
 }
 
 void MainFrame::on_sys_color_changed()
@@ -1083,6 +1090,8 @@ void MainFrame::on_sys_color_changed()
     MenuFactory::sys_color_changed(m_menubar);
 
     this->Refresh();
+
+    wxGetApp().searcher().dlg_sys_color_changed();
 }
 
 void MainFrame::update_mode_markers()
@@ -1437,7 +1446,7 @@ void MainFrame::init_menubar_as_editor()
 
         editMenu->AppendSeparator();
         append_menu_item(editMenu, wxID_ANY, _L("Searc&h") + "\tCtrl+F",
-            _L("Search in settings"), [this](wxCommandEvent&) { m_plater->search(m_plater->IsShown()); },
+            _L("Search in settings"), [this](wxCommandEvent&) { m_plater->IsShown() ? m_plater->search() : wxGetApp().show_search_dialog(); },
             "search", nullptr, []() {return true; }, this);
     }
 
@@ -2138,7 +2147,7 @@ SettingsDialog::SettingsDialog(MainFrame* mainframe)
 #else /* __APPLE__ */
                 case WXK_CONTROL_F:
 #endif /* __APPLE__ */
-                case 'F': { m_main_frame->plater()->search(false); break; }
+                case 'F': { wxGetApp().show_search_dialog(); break; }
                 default:break;
                 }
             }

src/slic3r/GUI/Preferences.cpp
@@ -191,7 +191,7 @@ static void activate_options_tab(std::shared_ptr<ConfigOptionsGroup> optgroup)
 	optgroup->parent()->Layout();
 
 	// apply sercher
-	wxGetApp().sidebar().get_searcher().append_preferences_options(optgroup->get_lines());
+	wxGetApp().searcher().append_preferences_options(optgroup->get_lines());
 }
 
 static void append_bool_option( std::shared_ptr<ConfigOptionsGroup> optgroup,
@@ -210,7 +210,7 @@ static void append_bool_option( std::shared_ptr<ConfigOptionsGroup> optgroup,
 	optgroup->append_single_option_line(option);
 
 	// fill data to the Search Dialog
-	wxGetApp().sidebar().get_searcher().add_key(opt_key, Preset::TYPE_PREFERENCES, optgroup->config_category(), L("Preferences"));
+	wxGetApp().searcher().add_key(opt_key, Preset::TYPE_PREFERENCES, optgroup->config_category(), L("Preferences"));
 }
 
 template<typename EnumType>
@@ -233,17 +233,18 @@ static void append_enum_option( std::shared_ptr<ConfigOptionsGroup> optgroup,
 	optgroup->append_single_option_line(option);
 
 	// fill data to the Search Dialog
-	wxGetApp().sidebar().get_searcher().add_key(opt_key, Preset::TYPE_PREFERENCES, optgroup->config_category(), L("Preferences"));
+	wxGetApp().searcher().add_key(opt_key, Preset::TYPE_PREFERENCES, optgroup->config_category(), L("Preferences"));
 }
 
 static void append_preferences_option_to_searcher(std::shared_ptr<ConfigOptionsGroup> optgroup,
 												const std::string& opt_key,
 												const wxString& label)
 {
+	Search::OptionsSearcher& searcher = wxGetApp().searcher();
 	// fill data to the Search Dialog
-	wxGetApp().sidebar().get_searcher().add_key(opt_key, Preset::TYPE_PREFERENCES, optgroup->config_category(), L("Preferences"));
+	searcher.add_key(opt_key, Preset::TYPE_PREFERENCES, optgroup->config_category(), L("Preferences"));
 	// apply sercher
-	wxGetApp().sidebar().get_searcher().append_preferences_option(Line(opt_key, label, ""));
+	searcher.append_preferences_option(Line(opt_key, label, ""));
 }
 
 void PreferencesDialog::build()

src/slic3r/GUI/Field.cpp
@@ -142,7 +142,7 @@ void Field::PostInitialize()
 #else /* __APPLE__ */
 				case WXK_CONTROL_F:
 #endif /* __APPLE__ */
-				case 'F': { wxGetApp().plater()->search(false); break; }
+				case 'F': { wxGetApp().show_search_dialog(); break; }
 			    default: break;
 			    }
 			    if (tab_id >= 0)

src/slic3r/GUI/GLCanvas3D.cpp
@@ -4765,7 +4765,12 @@ bool GLCanvas3D::_render_undo_redo_stack(const bool is_undo, float pos_x)
 // Getter for the const char*[] for the search list 
 static bool search_string_getter(int idx, const char** label, const char** tooltip)
 {
-    return wxGetApp().plater()->search_string_getter(idx, label, tooltip);
+    const Search::OptionsSearcher& search_list = wxGetApp().searcher();
+    if (0 <= idx && (size_t)idx < search_list.size()) {
+        search_list[idx].get_marked_label_and_tooltip(label, tooltip);
+        return true;
+    }
+    return false;
 }
 
 bool GLCanvas3D::_render_search_list(float pos_x)
@@ -4784,14 +4789,16 @@ bool GLCanvas3D::_render_search_list(float pos_x)
 	em *= m_retina_helper->get_scale_factor();
 #endif // ENABLE_RETINA_GL
 
-    Sidebar& sidebar = wxGetApp().sidebar();
+    // update searcher before show imGui search dialog on the plater, if printer technology or mode was changed
+    wxGetApp().check_and_update_searcher(wxGetApp().get_mode());
+    Search::OptionsSearcher& searcher = wxGetApp().searcher();
 
-    std::string& search_line = sidebar.get_search_line();
+    std::string& search_line = searcher.search_string();
     char *s = new char[255];
     strcpy(s, search_line.empty() ? _u8L("Enter a search term").c_str() : search_line.c_str());
 
     imgui->search_list(ImVec2(45 * em, 30 * em), &search_string_getter, s,
-        sidebar.get_searcher().view_params,
+        wxGetApp().searcher().view_params,
         selected, edited, m_mouse_wheel, wxGetApp().is_localized());
 
     search_line = s;
@@ -4800,7 +4807,7 @@ bool GLCanvas3D::_render_search_list(float pos_x)
         search_line.clear();
 
     if (edited)
-        sidebar.search();
+        searcher.search();
 
     if (selected >= 0) {
         // selected == 9999 means that Esc kye was pressed
@@ -4811,7 +4818,7 @@ bool GLCanvas3D::_render_search_list(float pos_x)
             sidebar.jump_to_option(selected);*/
         if (selected != 9999) {
             imgui->end(); // end imgui before the jump to option
-            sidebar.jump_to_option(selected);
+            wxGetApp().jump_to_option(selected);
             return true;
         }
         action_taken = true;

src/slic3r/GUI/Gizmos/GLGizmoCut.cpp
@@ -13,6 +13,7 @@
 #include "slic3r/GUI/Plater.hpp"
 #include "slic3r/GUI/GUI_ObjectManipulation.hpp"
 #include "slic3r/GUI/format.hpp"
+#include "slic3r/GUI/Field.hpp"
 #include "slic3r/Utils/UndoRedo.hpp"
 #include "libslic3r/AppConfig.hpp"
 #include "libslic3r/TriangleMeshSlicer.hpp"

src/slic3r/GUI/HintNotification.cpp
@@ -424,7 +424,7 @@ void HintDatabase::load_hints_from_file(const boost::filesystem::path& path)
 					std::string		opt = dict["hypertext_settings_opt"];
 					Preset::Type	type = static_cast<Preset::Type>(std::atoi(dict["hypertext_settings_type"].c_str()));
 					std::wstring	category = boost::nowide::widen(dict["hypertext_settings_category"]);
-					HintData		hint_data{ id_string, text1, weight, was_displayed, hypertext_text, follow_text, disabled_tags, enabled_tags, true, documentation_link, [opt, type, category]() { GUI::wxGetApp().sidebar().jump_to_option(opt, type, category); } };
+					HintData		hint_data{ id_string, text1, weight, was_displayed, hypertext_text, follow_text, disabled_tags, enabled_tags, true, documentation_link, [opt, type, category]() { GUI::wxGetApp().jump_to_option(opt, type, category); } };
 					m_loaded_hints.emplace_back(hint_data);
 				// open preferences
 				} else if(dict["hypertext_type"] == "preferences") {

commit 8c2f6d7b7dc00568f5ebc571df406c9ffacc619c

Fixed an update of Searcher on UI recreation. Bug was caused by fa86a3d0, when instance of searcher was moved to GuiApp and as result wasn't recreate on UI recreation. Searcher instance is in MainFrame now.

src/slic3r/GUI/GUI_App.cpp
@@ -1099,7 +1099,7 @@ void GUI_App::check_and_update_searcher(ConfigOptionMode mode /*= comExpert*/)
         if (tab->supports_printer_technology(print_tech))
             search_inputs.emplace_back(Search::InputInfo{ tab->get_config(), tab->type() });
 
-    m_searcher.check_and_update(print_tech, mode, search_inputs);
+    m_searcher->check_and_update(print_tech, mode, search_inputs);
 }
 
 void GUI_App::jump_to_option(const std::string& opt_key, Preset::Type type, const std::wstring& category)
@@ -1109,7 +1109,7 @@ void GUI_App::jump_to_option(const std::string& opt_key, Preset::Type type, cons
 
 void GUI_App::jump_to_option(size_t selected)
 {
-    const Search::Option& opt = m_searcher.get_option(selected);
+    const Search::Option& opt = m_searcher->get_option(selected);
     if (opt.type == Preset::TYPE_PREFERENCES)
         open_preferences(opt.opt_key(), into_u8(opt.group));
     else
@@ -1128,12 +1128,12 @@ void GUI_App::jump_to_option(const std::string& composite_key)
 
             // Regularly searcher is sorted in respect to the options labels,
             // so resort searcher before get an option
-            m_searcher.sort_options_by_key();
-            const Search::Option& opt = m_searcher.get_option(opt_key, tab->type());
+            m_searcher->sort_options_by_key();
+            const Search::Option& opt = m_searcher->get_option(opt_key, tab->type());
             tab->activate_option(opt_key, into_u8(opt.category));
 
             // Revert sort of searcher back
-            m_searcher.sort_options_by_label();
+            m_searcher->sort_options_by_label();
             break;
         }
     }
@@ -1142,7 +1142,7 @@ void GUI_App::jump_to_option(const std::string& composite_key)
 void GUI_App::show_search_dialog()
 {
     check_and_update_searcher(get_mode());
-    m_searcher.show_dialog();
+    m_searcher->show_dialog();
 }
 
 static int get_app_font_pt_size(const AppConfig* app_config)

src/slic3r/GUI/GUI_App.hpp
@@ -12,7 +12,7 @@
 #include "ConfigWizard.hpp"
 #include "OpenGLManager.hpp"
 #include "libslic3r/Preset.hpp"
-#include "Search.hpp"
+#include "I18N.hpp"
 
 #include <wx/app.h>
 #include <wx/colour.h>
@@ -39,6 +39,10 @@ class ModelObject;
 class PrintHostJobQueue;
 class Model;
 class AppUpdater;
+
+namespace Search {
+    class OptionsSearcher;
+}
 
 namespace GUI{
 
@@ -176,7 +180,7 @@ private:
     std::string m_instance_hash_string;
 	size_t m_instance_hash_int;
 
-    Search::OptionsSearcher m_searcher;
+    Search::OptionsSearcher* m_searcher{ nullptr };
 
 public:
     bool            OnInit() override;
@@ -191,7 +195,8 @@ public:
     bool is_recreating_gui() const { return m_is_recreating_gui; }
     std::string logo_name() const { return is_editor() ? "PrusaSlicer" : "PrusaSlicer-gcodeviewer"; }
 
-    Search::OptionsSearcher& searcher() noexcept { return m_searcher; }
+    Search::OptionsSearcher& searcher() noexcept { return *m_searcher; }
+    void                     set_searcher(Search::OptionsSearcher* searcher) { m_searcher = searcher; }
     void                     check_and_update_searcher(ConfigOptionMode mode = comExpert);
     void                     jump_to_option(size_t selected);
     void                     jump_to_option(const std::string& opt_key, Preset::Type type, const std::wstring& category);

src/slic3r/GUI/MainFrame.cpp
@@ -166,6 +166,8 @@ DPIFrame(NULL, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_S
     // Load the icon either from the exe, or from the ico file.
     SetIcon(main_frame_icon(wxGetApp().get_app_mode()));
 
+    wxGetApp().set_searcher(&m_searcher);
+
     // initialize tabpanel and menubar
     init_tabpanel();
     if (wxGetApp().is_gcode_viewer())

src/slic3r/GUI/MainFrame.hpp
@@ -27,6 +27,7 @@
 #include "GUI_Utils.hpp"
 #include "Event.hpp"
 #include "UnsavedChangesDialog.hpp"
+#include "Search.hpp"
 
 class wxBookCtrlBase;
 class wxProgressDialog;
@@ -93,6 +94,7 @@ class MainFrame : public DPIFrame
     wxSizer*    m_main_sizer{ nullptr };
 
     size_t      m_last_selected_tab;
+    Search::OptionsSearcher m_searcher;
 
     std::string     get_base_name(const wxString &full_name, const char *extension = nullptr) const;
     std::string     get_dir_name(const wxString &full_name) const;

src/slic3r/GUI/Preferences.cpp
@@ -20,6 +20,7 @@
 #include "OG_CustomCtrl.hpp"
 #include "GLCanvas3D.hpp"
 #include "ConfigWizard.hpp"
+#include "Search.hpp"
 
 #include "Widgets/SpinInput.hpp"
 