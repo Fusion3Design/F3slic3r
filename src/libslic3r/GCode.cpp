122d876bcad7c74e3da3eb50bb511febf3a7041c
andrewboktor <andrew.boktor@gmail.com>
2024-03-14 19:06:02 +0100

    SPE-2111: Rewrite the spiral vase with interpolation between layers.
    - Adds transition out to prevent a sharp edge at the top of the spiral vase.
    - Interpolation in the X and Y axes is used whether vase mode is enabled.
    - Maximum smoothing is automatically calculated from the nozzle diameter.
    
    This commit comes from https://github.com/prusa3d/PrusaSlicer/pull/12142 with some refactoring.
    
    Co-authored-by: Andrew Boktor <aboktor@microsoft.com>
    Co-authored-by: SoftFever <softfeverever@gmail.com>
    Co-authored-by: Vovodroid <vovodroid@users.noreply.github.com>
    Co-authored-by: Tom Glastonbury <t@tg73.net>
    Co-authored-by: Lukáš Hejl <hejl.lukas@gmail.com>

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -1509,11 +1509,12 @@ void GCodeGenerator::process_layers(
         });
     // The pipeline is variable: The vase mode filter is optional.
     const auto spiral_vase = tbb::make_filter<LayerResult, LayerResult>(slic3r_tbb_filtermode::serial_in_order,
-        [spiral_vase = this->m_spiral_vase.get()](LayerResult in) -> LayerResult {
+        [spiral_vase = this->m_spiral_vase.get(), &layers_to_print](LayerResult in) -> LayerResult {
             if (in.nop_layer_result)
                 return in;
             spiral_vase->enable(in.spiral_vase_enable);
-            return { spiral_vase->process_layer(std::move(in.gcode)), in.layer_id, in.spiral_vase_enable, in.cooling_buffer_flush};
+            bool last_layer = in.layer_id == layers_to_print.size() - 1;
+            return { spiral_vase->process_layer(std::move(in.gcode), last_layer), in.layer_id, in.spiral_vase_enable, in.cooling_buffer_flush};
         });
     const auto pressure_equalizer = tbb::make_filter<LayerResult, LayerResult>(slic3r_tbb_filtermode::serial_in_order,
         [pressure_equalizer = this->m_pressure_equalizer.get()](LayerResult in) -> LayerResult {
@@ -1602,11 +1603,12 @@ void GCodeGenerator::process_layers(
         });
     // The pipeline is variable: The vase mode filter is optional.
     const auto spiral_vase = tbb::make_filter<LayerResult, LayerResult>(slic3r_tbb_filtermode::serial_in_order,
-        [spiral_vase = this->m_spiral_vase.get()](LayerResult in)->LayerResult {
+        [spiral_vase = this->m_spiral_vase.get(), &layers_to_print](LayerResult in)->LayerResult {
             if (in.nop_layer_result)
                 return in;
             spiral_vase->enable(in.spiral_vase_enable);
-            return { spiral_vase->process_layer(std::move(in.gcode)), in.layer_id, in.spiral_vase_enable, in.cooling_buffer_flush };
+            bool last_layer = in.layer_id == layers_to_print.size() - 1;
+            return { spiral_vase->process_layer(std::move(in.gcode), last_layer), in.layer_id, in.spiral_vase_enable, in.cooling_buffer_flush };
         });
     const auto pressure_equalizer = tbb::make_filter<LayerResult, LayerResult>(slic3r_tbb_filtermode::serial_in_order,
         [pressure_equalizer = this->m_pressure_equalizer.get()](LayerResult in) -> LayerResult {
