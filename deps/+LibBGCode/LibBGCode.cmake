fad42fb8f3bc5fa8141b4ed14a07672f054855ef
enricoturri1966 <enricoturri@seznam.cz>
2024-03-18 16:10:06 +0100

    SPE-2183 - Modified LibBGCode.cmake to get latest version of libbgcode, where an out of bound access has been fixed into function MeatPack::unbinarize() (#12390)

--- deps/+LibBGCode/LibBGCode.cmake
+++ deps/+LibBGCode/LibBGCode.cmake
@@ -1,8 +1,8 @@
 set(LibBGCode_SOURCE_DIR "" CACHE PATH "Optionally specify local LibBGCode source directory")
 
 set(_source_dir_line
-    URL https://github.com/prusa3d/libbgcode/archive/6f43cb004ef3d3bda37dde49f6235e24d2717629.zip
-    URL_HASH SHA256=eb5198caecb6a693a294af6a56c37b0adb1eb159a34a9c3116970b80659ee9f9)
+    URL https://github.com/prusa3d/libbgcode/archive/638538cba6731eeafd47d332df70426d619532b5.zip
+    URL_HASH SHA256=14b652ae03a973979ee4c726c61a2c7ccd7c4fc1deb9ddab7b301cfb7225ef59)
 
 if (LibBGCode_SOURCE_DIR)
     set(_source_dir_line "SOURCE_DIR;${LibBGCode_SOURCE_DIR};BUILD_ALWAYS;ON")
