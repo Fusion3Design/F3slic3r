473f58c4e69c38aaf17ac60f7684cd746bed14f6
enricoturri1966 <enricoturri@seznam.cz>
2024-04-10 12:11:35 +0200

    SPE-2218 - libvgcode - Fixed detection of fan speed range

--- src/libvgcode/src/ViewerImpl.cpp
+++ src/libvgcode/src/ViewerImpl.cpp
@@ -1766,7 +1766,7 @@ void ViewerImpl::update_color_ranges()
                 m_volumetric_rate_range.update(round_to_bin(v.volumetric_rate()));
                 m_actual_volumetric_rate_range.update(round_to_bin(v.actual_volumetric_rate()));
             }
-            m_fan_speed_range.update(v.fan_speed);
+            m_fan_speed_range.update(round_to_bin(v.fan_speed));
             m_temperature_range.update(round_to_bin(v.temperature));
         }
         if ((v.is_travel() && m_settings.options_visibility.at(EOptionType::Travels)) ||
