e10e817390b623f207c72f6f38876ae468f7365e
Lukas Matena <lukasmatena@seznam.cz>
2024-06-21 22:36:12 +0200

    Bumped up version to 2.8.0-beta1

--- src/libslic3r/libslic3r.h
+++ src/libslic3r/libslic3r.h
@@ -18,8 +18,8 @@
 
 // Profiles for the alpha are stored into the PrusaSlicer-alpha directory to not mix with the current release.
 //#define SLIC3R_APP_FULL_NAME SLIC3R_APP_KEY
-  #define SLIC3R_APP_FULL_NAME SLIC3R_APP_KEY "-alpha"
-//#define SLIC3R_APP_FULL_NAME SLIC3R_APP_KEY "-beta"
+//#define SLIC3R_APP_FULL_NAME SLIC3R_APP_KEY "-alpha"
+#define SLIC3R_APP_FULL_NAME SLIC3R_APP_KEY "-beta"
 
 
 
