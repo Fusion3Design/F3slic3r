eb078d01d0ce07e4d5b7c9600e79737cbe41abf8
Jan Bartipan <barzto@gmail.com>
2024-06-24 16:58:13 +0200

    WebKit Credentials Challenge fixed passing data on linux

--- src/slic3r/GUI/WebViewPlatformUtilsLinux.cpp
+++ src/slic3r/GUI/WebViewPlatformUtilsLinux.cpp
@@ -11,21 +11,32 @@ struct Credentials {
     std::string password;
 };
 
-using WebViewCredentialsMap = std::unordered_map<void*, std::unique_ptr<Credentials>>;
-WebViewCredentialsMap g_credentials;
-
 gboolean webkit_authorize_handler(WebKitWebView *web_view, WebKitAuthenticationRequest *request, gpointer user_data)
 {
     const Credentials& creds = *static_cast<const Credentials*>(user_data);
-    webkit_authentication_request_authenticate(request, webkit_credential_new(creds.username.c_str(), creds.password.c_str()));
+    webkit_authentication_request_authenticate(request, webkit_credential_new(creds.username.c_str(), creds.password.c_str(), WEBKIT_CREDENTIAL_PERSISTENCE_FOR_SESSION));
+    return TRUE;
 }
 
-    void setup_webview_with_credentials(wxWebView* web_view, const std::string& username, const std::string& password)
+void free_credentials(gpointer user_data, GClosure* closure)
 {
-    std::unique_ptr<Credentials> ptr(new Credentials{username, password});
-    g_credentials[web_view->GetNativeBackend()] = std::move(ptr);
-    g_signal_connect(web_view, "authorize",
-                     G_CALLBACK(webkit_authorize_handler), g_credentials[web_view].get());
+    Credentials* creds = static_cast<Credentials*>(user_data);
+    delete creds;
+}
+
+void setup_webview_with_credentials(wxWebView* web_view, const std::string& username, const std::string& password)
+{
+    WebKitWebView* native_backend = static_cast<WebKitWebView *>(web_view->GetNativeBackend());
+    Credentials* user_data = new Credentials{username, password};
+
+    g_signal_connect_data(
+        native_backend,
+        "authenticate",
+        G_CALLBACK(webkit_authorize_handler),
+        user_data,
+        &free_credentials,
+        static_cast<GConnectFlags>(0)
+    );
 
 }
 
