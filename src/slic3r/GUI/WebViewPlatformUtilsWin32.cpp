fc76ec00f1c3b5e9c453493ecffb1deff4444715
Jan Bařtipán <barzto@gmail.com>
2024-07-02 17:02:01 +0200

    Patch wxWidget CMake to download and use recent WebView2 library

--- src/slic3r/GUI/WebViewPlatformUtilsWin32.cpp
+++ src/slic3r/GUI/WebViewPlatformUtilsWin32.cpp
@@ -4,6 +4,7 @@
 #include "WebView2.h"
 #include <wrl.h>
 #include <atlbase.h>
+#include <boost/log/trivial.hpp>
 
 #include "wx/msw/private/comptr.h"
 
@@ -15,59 +16,39 @@ namespace Slic3r::GUI {
 
 void setup_webview_with_credentials(wxWebView* webview, const std::string& username, const std::string& password)
 {
-    ICoreWebView2 *webView2 = (ICoreWebView2 *) webview->GetNativeBackend();
+    ICoreWebView2 *webView2 = static_cast<ICoreWebView2 *>(webview->GetNativeBackend());
 
-    
-    wxCOMPtr<ICoreWebView2_2> webview2Com;
-    wxCOMPtr<ICoreWebView2Environment> environment;
-    if(FAILED(webView2->QueryInterface(IID_PPV_ARGS(&webview2Com))))
-    {
-        return;
-    }
-    if (FAILED(webview2Com->get_Environment(&environment))) {
-        return;
-    }
 
-    LPWSTR wideString;
-    if (SUCCEEDED(environment->get_BrowserVersionString(&wideString))) {
-        std::wcout << L"WebView2 runtime version: " << wideString << std::endl;
-    }
-
-    
     wxCOMPtr<ICoreWebView2_10> wv2_10;
-    //if (!SUCCEEDED(webView2->QueryInterface(IID_PPV_ARGS(&wv2_10)))) {
-    //wv2_10 = wil::com_ptr<ICoreWebView2>(m_appWindow->GetWebView()).query<ICoreWebView2_2>();
-    // auto webView10 = webView2->query<ICoreWebView2_10>()
     HRESULT hr = webView2->QueryInterface(IID_PPV_ARGS(&wv2_10));
     if (FAILED(hr)) {
         return;        
     }
+
     // should it be stored?
     EventRegistrationToken basicAuthenticationRequestedToken = {};
 
-    if (!SUCCEEDED(wv2_10->add_BasicAuthenticationRequested(
+    if (FAILED(wv2_10->add_BasicAuthenticationRequested(
             Microsoft::WRL::Callback<ICoreWebView2BasicAuthenticationRequestedEventHandler>(
                 [username, password](ICoreWebView2 *sender, ICoreWebView2BasicAuthenticationRequestedEventArgs *args) {
                     wxCOMPtr<ICoreWebView2BasicAuthenticationResponse> basicAuthenticationResponse;
-                    if (!SUCCEEDED(args->get_Response(&basicAuthenticationResponse))) {
+                    if (FAILED(args->get_Response(&basicAuthenticationResponse))) {
                         return -1;
                     }
-                    if (!SUCCEEDED(basicAuthenticationResponse->put_UserName(GUI::from_u8(username).c_str()))) {
+                    if (FAILED(basicAuthenticationResponse->put_UserName(GUI::from_u8(username).c_str()))) {
                         return -1;
                     }
-                    if (!SUCCEEDED(basicAuthenticationResponse->put_Password(GUI::from_u8(password).c_str()))) {
+                    if (FAILED(basicAuthenticationResponse->put_Password(GUI::from_u8(password).c_str()))) {
                         return -1;
                     }
                     return 0;
                 }
             ).Get(),
             &basicAuthenticationRequestedToken
-        ))) 
-    {
+        ))) {
 
+        BOOST_LOG_TRIVIAL(error) << "WebView: Cannot register authentication request handler";
     }
-    wv2_10->Release();
-    
        
 }
 #endif
