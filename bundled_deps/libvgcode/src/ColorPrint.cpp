4ae7a7c885ed04e0d72df11672c4161612269b09
Lukas Matena <lukasmatena@seznam.cz>
2024-07-08 15:33:44 +0200

    Moved libvgcode to src for now (to avoid conflicts)

--- bundled_deps/libvgcode/src/ColorPrint.cpp
+++
@@ -1,8 +0,0 @@
-///|/ Copyright (c) Prusa Research 2023 Enrico Turri @enricoturri1966, Pavel Mikuš @Godrak
-///|/
-///|/ libvgcode is released under the terms of the AGPLv3 or higher
-///|/
-#include "libvgcode/ColorPrint.hpp"
-
-namespace libvgcode {
-} // namespace libvgcode
